2c73b0f1930c1189585157e6c5acc7dfc7f3717d
Ivan <ivanakazver3d@gmail.com>
2018-08-26 20:55:14 +0700

    Quick change: added ability to create categories right on the categories list page

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -8,24 +8,44 @@
     </div>
     @endif
 
-    <h1>Categories</h1>
+    <h1 class="text-center">Categories</h1>
 
-    <table class="table">
-        <thead>
-            <tr>
-                <th>Id</th>
-                <th>Name</th>
-            </tr>
-        </thead>
-        <tbody>
-            @if($categories)
-                @foreach ($categories as $category)
-                    <tr>
-                        <td>{{ $category->id }}</td>
-                        <td><a href="{{ route('categories.edit', $category->id) }}">{{ $category->name }}</a></td>
-                    </tr>
-                @endforeach
-            @endif
-        </tbody>
-    </table>
+    <div class="col-md-6">
+        <h3 class="text-center">Create Category</h3>
+
+        {!! Form::open(['method'=>'POST', 'action'=>'AdminCategoriesController@store']) !!}
+        <div class="form-group">
+            {!! Form::text('name', null, ['class'=>'form-control']) !!}
+        </div>
+    
+        <div class="form-group">
+            {!! Form::submit('Create Category', ['class'=>'btn btn-primary']) !!}
+        </div>
+    
+        {!! Form::close() !!}
+    
+        @include('admin.error')
+    </div>
+
+    <div class="col-md-6">
+        <h3 class="text-center">Categories List</h3>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Name</th>
+                </tr>
+            </thead>
+            <tbody>
+                @if($categories)
+                    @foreach ($categories as $category)
+                        <tr>
+                            <td>{{ $category->id }}</td>
+                            <td><a href="{{ route('categories.edit', $category->id) }}">{{ $category->name }}</a></td>
+                        </tr>
+                    @endforeach
+                @endif
+            </tbody>
+        </table>
+    </div>
 @endsection
